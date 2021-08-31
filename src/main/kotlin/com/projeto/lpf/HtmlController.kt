75316dc9f4a6722f757cd48cc6411a3d888494fa
Valdery Alves Paes Junior <valdery.jur@gmail.com>
2021-08-31 00:43:14 -0300

    Initial commit

---
+++ src/main/kotlin/com/projeto/lpf/HtmlController.kt
@@ -0,0 +1,18 @@
+package com.projeto.lpf
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HtmlController {
+
+  @GetMapping("/")
+  fun blog(model: Model): String {
+    model["title"] = "Página"
+    // Página a ser carregada
+    return "page"
+  }
+
+}
