de350d2405bdda95cba374a79fa22a8363af8ac6
Bruna Lins <blg@ecomp.poli.br>
2021-09-04 19:32:29 -0300

    Removes unused endpoints
    Signed-off-by: Bruna Lins <blg@ecomp.poli.br>

--- src/main/kotlin/com/projeto/lpf/HtmlController.kt
+++ src/main/kotlin/com/projeto/lpf/HtmlController.kt
@@ -4,12 +4,14 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import java.io.File
+
 
 @Controller
 class HtmlController {
 
   @GetMapping("/")
-  fun blog(model: Model): String {
+  fun webPage(model: Model): String {
     model["title"] = "Página"
     // Página a ser carregada
     return "page"
